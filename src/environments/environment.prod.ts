8b69d5bf0aaadd0717eee7e22bb990b5b4517c98
ivangreveagree <igreve@agreemarket.com>
2022-08-07 15:06:43 -0300

    .

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  api: {
-    baseUrl: 'https://gateway.marvel.com/v1/public',
-    publicKey: '28e7a602eb43aa65ce252fbe752ff8c5',
-    privateKey: 'ada277975cca0d8e062f21a9c431e17d3f4d7cb3'
-  },
-  production: true
-};
